887a60c5666a3d54182bf2c06ea2a629859610d2
Angel Campos Muñoz <angel.campos.m@outlook.com>
2020-09-04 02:20:16 +0200

    updated LOGGER.class.ps1

--- src/LOGGER.clas.ps1
+++ src/LOGGER.clas.ps1
@@ -13,6 +13,7 @@ Class LOGGER
 {
     #region PROPERTIES
 
+    hidden [string]$channel;
     hidden [bool]$daily;
     hidden [System.Collections.ArrayList]$items;
     hidden [string]$level;
@@ -26,6 +27,7 @@ Class LOGGER
 
     LOGGER()
     {
+        $this.channel = 'log';
         $this.daily = $false;
         $this.items = [System.Collections.ArrayList]@()
         $this.level = $null;
@@ -55,15 +57,15 @@ Class LOGGER
     hidden [string]getFileName()
     {
         if ($this.daily) {
-            return 'incidencias_' + $(Get-Date -Format 'yyyy_MM_dd') + '.log';
+            return "$($this.channel)_$(Get-Date -Format 'yyyy_MM_dd').log";
         }
 
-        return 'incidencias.log';
+        return "$($this.channel).log";
     }
-    
+
     hidden [string]getHeader()
     {
-        return '[' + $(Get-Date -Format 'yyyy/MM/dd H:mm:ss') + ']';
+        return '[' + $(Get-Date -Format 'yyyy/MM/dd HH:mm:ss') + ']';
     }
 
     hidden [string]getLevel()
@@ -73,6 +75,41 @@ Class LOGGER
         }
 
         return '[' + $this.level + ']';
+    }
+
+    hidden [string]getLogFile()
+    {
+        return "$($this.path)$($this.getFileName())";
+    }
+
+    hidden [string]getLogMessage()
+    {
+        [LOGGER]::checkPath($this.path);
+
+        $msg = $this.getHeader();
+
+        if (($this.getLevel()).Length -gt 0) 
+        {
+            $msg += $this.getLevel();
+        }
+
+        if ([string]::IsNullOrEmpty($this.message)) 
+        {
+            $msg += ' ' + $($this.items -join $this.separator);
+        }
+        else 
+        {
+            $msg += ' ' + $this.message;
+        }
+
+        return $msg;
+    }
+
+    [LOGGER]setChannel([string]$channel)
+    {
+        $this.channel = $channel;
+
+        return $this;
     }
         
     [LOGGER]setDaily()
@@ -110,28 +147,17 @@ Class LOGGER
         return $this;
     }
 
+    [string]toString()
+    {
+        return $this.getLogMessage();
+    }
+
     [void]Write()
     {
-        [LOGGER]::checkPath($this.path);
-
-        $msg = $this.getHeader();
-
-        if (($this.getLevel()).Length -gt 0) 
-        {
-            $msg += $this.getLevel();
-        }
-
-        if ([string]::IsNullOrEmpty($this.message)) 
-        {
-            $msg += ' ' + $($this.items -join $this.separator);
-        }
-        else 
-        {
-            $msg += ' ' + $this.message;
-        }
-
-        $msg | Out-File -FilePath $($this.path + $this.getFileName()) -Encoding utf8 -Append
+        $this.getLogMessage() | Out-File -FilePath $($this.getLogFile()) -Encoding utf8 -Append
     }
 
     #endregion
 }
+
+[LOGGER]::New().setChannel('mensajes').addItem('usuario', 'yomismo').toString()
